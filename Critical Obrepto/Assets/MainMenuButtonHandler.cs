4ea533adbde06093781d822c0f7ee1d730aac44c
Lobo-Prix <lobo_prix@naver.com>
2016-06-03 20:57:44 +0900

    save

---
+++ Critical Obrepto/Assets/MainMenuButtonHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonHandler : MonoBehaviour
+{
+    public void OnHelp()
+    { }
+
+    public void OnStart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnRecords()
+    { }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+}
